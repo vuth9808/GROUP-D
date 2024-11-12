b5edddedef26171366e6ea8256d9f8397d5fb13a
huyvu0607 <vudh1092@ut.edu.vn>
2024-11-12 21:43:04 +0700

    them code

--- FengShuiKoi.Reponsitories/obj/Debug/net8.0/FengShuiKoi.Reponsitories.AssemblyInfo.cs
+++ FengShuiKoi.Reponsitories/obj/Debug/net8.0/FengShuiKoi.Reponsitories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FengShuiKoi.Reponsitories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54f887d9be4f0d9ace264674adaf5515ddf66f9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FengShuiKoi.Reponsitories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FengShuiKoi.Reponsitories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
