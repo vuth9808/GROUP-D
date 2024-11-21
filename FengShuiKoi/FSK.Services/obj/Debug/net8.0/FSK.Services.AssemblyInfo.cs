1178574b0929b95ad64db1e55cdbb410c9da999e
huyvu0607 <vudh1092@ut.edu.vn>
2024-11-21 13:07:12 +0700

    d

--- FengShuiKoi/FSK.Services/obj/Debug/net8.0/FSK.Services.AssemblyInfo.cs
+++ FengShuiKoi/FSK.Services/obj/Debug/net8.0/FSK.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FSK.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bfe7492f8a8c2e8dc393b5d914d9a9f689634ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("FSK.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FSK.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FengShuiKoi/FSK.WebApp/obj/Debug/net8.0/FSK.WebApp.AssemblyInfo.cs
+++ FengShuiKoi/FSK.WebApp/obj/Debug/net8.0/FSK.WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FSK.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bfe7492f8a8c2e8dc393b5d914d9a9f689634ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("FSK.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FSK.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
